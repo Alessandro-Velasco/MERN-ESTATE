7d3c4a4bc37b832c5bb81c3674065789accfdb83
Alessandro <velasco.alessandro@gmail.com>
2024-01-22 11:57:05 +0100

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,9 +1,23 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+mongoose
+
+.connect(process.env.MONGO)
+.then(() => {
+    console.log('Connected to MongoDB');
+}) 
+.catch((err) => {
+    console.log(err);
+})
 
 const app = express();
 
-app.listen(5173, () => {
-    console.log('Server is running on port 5173')
+app.listen(3000, () => {
+    console.log('Server is running on port 3000')
     }
 
 );
