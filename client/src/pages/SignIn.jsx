0eb46ca0397ed128cac858dd8900e33570a1ff15
Alessandro <velasco.alessandro@gmail.com>
2024-01-29 12:50:37 +0100

    add react redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { setUser } from '../redux/userSlice';
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
