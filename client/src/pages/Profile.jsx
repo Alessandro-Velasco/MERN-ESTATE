50a71b3e93275362e0b216730d1c5dff41797bc1
Alessandro <velasco.alessandro@gmail.com>
2024-02-06 22:24:05 +0100

    complete image upload funcionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,45 +1,70 @@
-import {useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
 import { useRef, useState, useEffect } from 'react';
-import { getStorage, ref, uploadBytesResumable} from 'firebase/storage';
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 import { app } from '../firebase';
 
 export default function Profile() {
   const fileRef = useRef(null);
   const { currentUser } = useSelector((state) => state.user);
   const [file, setFile] = useState(undefined);
-  console.log(file);
+  const [filePerc, setFilePerc] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const [formData, setFormData] = useState({
+    avatar: currentUser.avatar // Set initial avatar from currentUser
+  });
 
-//firebase storage
-//allow read;
-//allow write: if
-//request.source.size < 2 * 1024 * 1024 && 
-//request.resource.contentType.matches('image/.*')
-    
-useEffect(() => {
-  if (file) {
-    handleFileUpload(file);
-  }
-}, [file]);
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
 
-const handleFileUpload = (file) => {
-  const storage = getStorage(app);
-  const fileName = new Date().getTime() + file.name;
-  const storageRef = ref(storage, fileName);
-  const uploadTask = uploadBytesResumable(storageRef, file);
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
 
-  uploadTask.on('state_changed',
-  (snapshot) => {
-    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-    console.log('Upload is' + progress + '% done');
-  })
-}
+    uploadTask.on('state_changed',
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFilePerc(Math.round(progress));
+      },
+      (error) => {
+        setFileUploadError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref)
+          .then((downloadURL) => {
+            setFormData({ ...formData, avatar: downloadURL }); // Update avatar URL in formData
+          })
+          .catch((error) => {
+            console.error('Error getting download URL:', error);
+          });
+      }
+    );
+  };
+  
+
     
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
       <form className='flex flex-col gap-4'>
         <input onChange={(e) => setFile(e.target.files[0])} type="file" ref={fileRef} hidden accept='image/*' />
-        <img onClick={()=>fileRef.current.click()} src={currentUser.avatar} alt="profile" className='rounded-full h-24 object-cover cursor-pointer self-center mt-2' />
+        <img onClick={()=>fileRef.current.click()} src={formData.avatar || currentUser.avatar} alt="profile" className='rounded-full h-24 object-cover cursor-pointer self-center mt-2' />
+        <p className='text-sm self-center'>
+          {fileUploadError ?
+          (<span className='text-red-700'>Error image upload(image must be less than 2mb)</span>) :
+          filePerc > 0 && filePerc < 100 ? (
+            <span className='text-slate-700'>{`Uploading ${filePerc}%`}</span>) :
+            filePerc === 100 ? (
+              <span className='text-green-700'>Image successfully uploaded!</span>)
+              : (
+              ''
+          )}
+          
+        </p>
         <input type="text" placeholder='username' id='username' className='border p-3 rounded-lg' />
         <input type="text" placeholder='email' id='email' className='border p-3 rounded-lg' />
         <input type="text" placeholder='password' id='password' className='border p-3 rounded-lg' />
